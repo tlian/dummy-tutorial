94217e7602fd6f66260e513a60e20694143bc9ff
Thawngzapum Lian <thawngv@gmail.com>
2024-10-11 09:22:48 -0400

    Update: added some comments

--- Python/flask-html/app.py
+++ Python/flask-html/app.py
@@ -3,15 +3,20 @@
 
 from flask import Flask, request, render_template
 
+# Create a Flask app
 app = Flask(__name__)
 
+# Define a route
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    # If the request method is POST, it means the form has been submitted.
     if request.method == 'POST':
+        # Extract value from the form data defined in templates/form.html data
         name = request.form['name']
         email = request.form['email']
         phone = request.form['phone']
         address = request.form['address']
+        # Return the data in HTML format so that it's more easier to read
         return f"<p>Name: {name}</p><p>Email: {email}</p><p>Phone:{phone}</p><p>Address:{address}<p>"
     return render_template('form.html')
 
